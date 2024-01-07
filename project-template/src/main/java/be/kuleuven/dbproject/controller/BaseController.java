a55ac664035e9aaf60c3813466d93b33ae257130
unknown <nordin.ben-al-lal@student.uhasselt.be>
2024-01-07 14:42:46 +0100

    logout btn

--- project-template/src/main/java/be/kuleuven/dbproject/controller/BaseController.java
+++ project-template/src/main/java/be/kuleuven/dbproject/controller/BaseController.java
@@ -1,12 +1,26 @@
 package be.kuleuven.dbproject.controller;
 
+import be.kuleuven.dbproject.ScreenFactory;
 import be.kuleuven.dbproject.model.Employee;
 import be.kuleuven.dbproject.view.BaseView;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class BaseController {
+    @FXML
+    private Button btnLogOut;
+
+    @FXML
+    void initialize(){
+        btnLogOut.setOnAction(e -> {
+           new ScreenFactory("login");
+           view.stop();
+        });
+
+    }
+
     private BaseView view;
     private Employee employeeLoggedIn;
-
     public BaseController(BaseView view, Employee employeeLoggedIn) {
         this.view = view;
         this.employeeLoggedIn = employeeLoggedIn;
